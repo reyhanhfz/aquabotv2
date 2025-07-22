b8e6c9846346ef7a1b65c646e6ffd631d0bdb039
reyhanhfz <muhammad.rayhan.nasution@wbi.ac.id>
2025-07-22 15:23:04 +0700

    bismillah

--- aquabot/aquabot.ino
+++ aquabot/aquabot.ino
@@ -3,7 +3,7 @@
 #include <BlynkSimpleEsp8266.h>
 #include <NewPing.h>
 #include <Servo.h>
-#include "secrets.h"
+#include "./secrets.h"
 
 #define TRIG_PIN D2
 #define ECHO_PIN D1
@@ -19,9 +19,11 @@ BlynkTimer timer;
 Servo myServo;
 bool pompaStatus = false;
 
-void sendDistance() {
+void sendDistance()
+{
   int distance = sonar.ping_cm();
-  if (distance == 0 || distance > TINGGI_TANGKI + 10) return;
+  if (distance == 0 || distance > TINGGI_TANGKI + 10)
+    return;
 
   int tinggiAir = TINGGI_TANGKI - distance;
   tinggiAir = constrain(tinggiAir, 0, TINGGI_TANGKI);
@@ -31,12 +33,15 @@ void sendDistance() {
   Blynk.virtualWrite(V1, tinggiAir);
   Blynk.virtualWrite(V4, persenKetinggian);
 
-  if (tinggiAir < BATAS_NYALA && !pompaStatus) {
+  if (tinggiAir < BATAS_NYALA && !pompaStatus)
+  {
     digitalWrite(RELAY_PIN, LOW);
     pompaStatus = true;
     Blynk.virtualWrite(V2, "Pompa AKTIF (Otomatis)");
     Blynk.virtualWrite(V3, 1);
-  } else if (tinggiAir > BATAS_MATI && pompaStatus) {
+  }
+  else if (tinggiAir > BATAS_MATI && pompaStatus)
+  {
     digitalWrite(RELAY_PIN, HIGH);
     pompaStatus = false;
     Blynk.virtualWrite(V2, "Pompa MATI (Otomatis)");
@@ -44,19 +49,22 @@ void sendDistance() {
   }
 }
 
-BLYNK_WRITE(V3) {
+BLYNK_WRITE(V3)
+{
   int state = param.asInt();
   digitalWrite(RELAY_PIN, state ? LOW : HIGH);
   pompaStatus = state;
   Blynk.virtualWrite(V2, state ? "Pompa AKTIF (Manual)" : "Pompa MATI (Manual)");
 }
 
-BLYNK_WRITE(V5) {
+BLYNK_WRITE(V5)
+{
   int sudut = constrain(param.asInt(), 0, 180);
   myServo.write(sudut);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);
@@ -66,7 +74,8 @@ void setup() {
   myServo.write(0);
 }
 
-void loop() {
+void loop()
+{
   Blynk.run();
   timer.run();
 }
